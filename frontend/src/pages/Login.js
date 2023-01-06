2960c8935f63f11f4cf03718e5d0bfc3789b515a
楊喆東 <yangchetung@yangzhedongdeMacBook-Air.local>
2023-01-06 14:31:38 +0800

    connect

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useMetaMask } from "metamask-react";
 
 const Login = ({ setLogin }) => {
-  const [password, setPassword] = useState("");
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
+  console.log(status);
+
+  useEffect(() => {
+    if (status === "connected") setLogin(true);
+  }, [status]);
+  return (
+    <div className="loginPage" style={{ background: "black", color: "white" }}>
+      <button className="connect" onClick={() => connect()}>
+        cononect
+      </button>
+    </div>
+  );
 };
+
+export default Login;
