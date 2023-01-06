26740d9fc40e2992cbf129ef37c094c791b55b0c
Lawrence Tsai <tsailawrence812@gmail.com>
2023-01-06 15:04:23 +0800

    update login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useMetaMask } from "metamask-react";
+import { Button, Checkbox, Form, Input, Layout } from "antd";
+import logo from "../img/ChainNote_white.png";
+
+const { Header, Content } = Layout;
 
 const Login = ({ setLogin }) => {
   const { status, connect, account, chainId, ethereum } = useMetaMask();
@@ -9,11 +13,49 @@ const Login = ({ setLogin }) => {
     if (status === "connected") setLogin(true);
   }, [status]);
   return (
-    <div className="loginPage" style={{ background: "black", color: "white" }}>
-      <button className="connect" onClick={() => connect()}>
-        cononect
-      </button>
-    </div>
+    <Content
+      className="site-layout-background"
+      style={{
+        padding: 100,
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "black",
+      }}
+    >
+      <div
+        className="logo"
+        style={{
+          display: "flex",
+          justifyContent: "Center",
+          height: 200,
+          // marginTop: 50,
+          marginBottom: 20,
+        }}
+      >
+        <img src={logo}></img>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Button
+          className="connect"
+          onClick={() => connect()}
+          style={{
+            borderRadius: "50px",
+            width: "30%",
+            height: "120%",
+          }}
+        >
+          Cononect
+        </Button>
+      </div>
+    </Content>
   );
 };
 
